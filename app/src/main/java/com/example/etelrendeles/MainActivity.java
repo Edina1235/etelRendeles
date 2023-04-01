f15b2acc975f85ab3c1a0d219cb9b6a6b7d3b3dc
Tóth Edina <h165414@stud.u-szeged.hu>
2023-04-01 11:36:33 +0200

    activities

--- app/src/main/java/com/example/etelrendeles/MainActivity.java
+++ app/src/main/java/com/example/etelrendeles/MainActivity.java
@@ -2,13 +2,49 @@ package com.example.etelrendeles;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int AHA=0;
+    private static final String LOG = MainActivity.class.getName();
+    EditText email;
+    EditText jelszo;
+
+    private SharedPreferences sharedPreferences;
+    private FirebaseAuth firebaseAuth;
+    private GoogleSignInClient googleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_regisztracio);
+        setContentView(R.layout.activity_main);
+        email=findViewById(R.id.email);
+        jelszo=findViewById(R.id.jelszo);
+    }
+
+    public void bejelentkezes(View view) {
+        Log.d(LOG, "bejelentkezes");
+    }
+
+    public void regisztracio(View view) {
+        Intent intent = new Intent(this, RegisztracioActivity.class);
+        Log.d(LOG, "regisztracio");
+        startActivity(intent);
+    }
+
+    public void google(View view) {
+        Log.d(LOG, "google");
+    }
+
+    public void vendeg(View view) {
+        Log.d(LOG, "vendeg");
     }
 }
--- app/src/main/java/com/example/etelrendeles/RegisztracioActivity.java
+++ app/src/main/java/com/example/etelrendeles/RegisztracioActivity.java
@@ -1,4 +1,23 @@
 package com.example.etelrendeles;
 
-public class RegisztracioActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisztracioActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regisztracio);
+
+    }
+
+    public void megse(View view) {
+        finish();
+    }
+
+
 }
